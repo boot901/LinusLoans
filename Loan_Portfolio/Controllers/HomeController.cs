cdc62d9029645a601835050d17f441101e985665
boost90 <linus@trahair.net>
2016-08-11 11:53:33 +0100

    Revert "thing"
    This reverts commit e2884cb6233770e3cb37b8f544b9d5cab75e9272.

--- Loan_Portfolio/Controllers/HomeController.cs
+++ Loan_Portfolio/Controllers/HomeController.cs
@@ -13,21 +13,33 @@ namespace Loan_Portfolio.Controllers
 {
 	public class HomeController : Controller
 	{
+		public SearchModel m;
+
 		public string CodeText { get; set; }
 		public HomeController() {
 			CodeText = "why refuse";
+			m = new SearchModel();
+			m.approval = "All";
+			m.groups = "In Group";
+			m.numpage = 20;
+			m.searcher = null;
 		}
-		public ActionResult Index()
-		{
-			if (ViewData["searchy"] != null)
-			{
-			}
-			else {
-				ViewData["searchy"]=null;
-			}
-			return View();
+		[HttpGet]
+		public ActionResult Index(string numby,string app, string grp, string srch) {
+			if (app != null) { m.approval = app; }
+			if (grp != null) { m.groups = grp; }
+			if (srch != null) { m.searcher = srch; }
+			if (numby != null) { m.numpage = Convert.ToInt32(numby); }
+			ViewData["Mod"] = m;
+			ViewData["searchy"] = m.searcher;
+			string file = System.IO.File.ReadAllText(@"\path.json");
+			List<DocDetail> frm = JsonConvert.DeserializeObject<List<DocDetail>>(file);
+			ViewBag.list = frm;	
+			ViewBag.app = m.approval;
 
+			return View();
 		}
+
 		[Route("Mana")]
 		public ActionResult Mana(DocDetail d)
 		{
@@ -37,15 +49,27 @@ namespace Loan_Portfolio.Controllers
 		}
 		[HttpPost]
 		[ActionName("Index")]
-		public ActionResult IndexPost(SearchModel s){
+		public ActionResult IndexPost(SearchModel s)
+		{
 			string file = System.IO.File.ReadAllText(@"\path.json");
 			List<DocDetail> frm = JsonConvert.DeserializeObject<List<DocDetail>>(file);
-			ViewData["searchy"]= s.searcher;
-
+		
+			m = s;
+			ViewData["Mod"] = m;
+			ViewData["searchy"] = m.searcher;
 			ViewBag.list = frm;
 			ViewBag.date = DateTime.Now.ToShortDateString();
 			ViewBag.app = s.approval;
 			return View();
+		}
+		[Route("Duplicate")]
+		public ActionResult Duplicate(int idNum,string searcher,string approval,int numpage,string groups) {
+			string file = System.IO.File.ReadAllText(@"\path.json");
+			List<DocDetail> frm = JsonConvert.DeserializeObject<List<DocDetail>>(file);
+			frm.Add(frm[idNum]);
+			string json = JsonConvert.SerializeObject(frm);
+			System.IO.File.WriteAllText(@"\path.json", json);
+			return RedirectToAction("Index", "Home", new { app=(""+approval),  grp=("" + groups), srch=(searcher+""),  num=(""+numpage) });
 		}
 	}
 	}
---
+++ Loan_Portfolio/Helpers/Expression.cs
@@ -0,0 +1,6 @@
+﻿namespace System.Web.Mvc.Html
+{
+	public class Expression<T1, T2, T3, T4, T5, T6, T7, T8, T9, vals, T10>
+	{
+	}
+}
